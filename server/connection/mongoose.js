cd653165e5903289bc2240caa3f6462f259871b8
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-06-16 19:52:35 +0530

    Delete server directory

--- server/connection/mongoose.js
+++
@@ -1,8 +0,0 @@
-// connection/mongoose.js
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const PORT = process.env.PORT || 5555;
-export const mongoDBURL = process.env.mongoDBURL || 'your-mongodb-connection-string-here';
-export const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
