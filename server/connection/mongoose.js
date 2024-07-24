3b4b5eb3a61b12fe9e5dcc7e9d6cfe4cd093db3d
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-07-24 11:50:53 +0530

    Update mongoose.js

--- server/connection/mongoose.js
+++ server/connection/mongoose.js
@@ -3,6 +3,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT || 5555;
+export const PORT = process.env.PORT;
 export const mongoDBURL = process.env.mongoDBURL || 'your-mongodb-connection-string-here';
 export const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
